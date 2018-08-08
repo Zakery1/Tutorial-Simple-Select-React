9184b30c61922e3f051bd4a7f2eef06baa775747
Zach Kadish <zach.kadish@nextiva.com>
2018-08-08 10:44:25 -0700

    commented out options var in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,11 +5,11 @@ import React from 'react';
 
 import './app.scss';
 
-const options = [
-  { name: 'option1', value: '1' },
-  { name: 'option2', value: '2' },
-  { name: 'option3', value: '3' },
-];
+// const options = [
+//   { name: 'option1', value: '1' },
+//   { name: 'option2', value: '2' },
+//   { name: 'option3', value: '3' },
+// ];
 
 const App = () => (
   <div className="development">
