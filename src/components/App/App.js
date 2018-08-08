f7b9dfc582260604a8b02d726adb2e5a5374b9bd
Zach Kadish <zach.kadish@nextiva.com>
2018-08-08 10:19:47 -0700

    commented out unused code in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 // import NpmSelect from 'simple-react-select';
-import Select from 'src/components/Select';
+// import Select from 'src/components/Select';
 
 import './app.scss';
 
-const options = [
-  { name: 'option1', value: '1' },
-  { name: 'option2', value: '2' },
-  { name: 'option3', value: '3' },
-];
+// const options = [
+//   { name: 'option1', value: '1' },
+//   { name: 'option2', value: '2' },
+//   { name: 'option3', value: '3' },
+// ];
 
 const App = () => (
   <div className="development">
